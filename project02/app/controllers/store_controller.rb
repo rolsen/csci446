b50a54b34f6a8c4c11e88759b08a6d1f0f7be14a
Rory Olsen <rolsen@mines.edu>
2011-02-01 23:48:17 -0700

    Fixed Add To Cart button functionality

--- project02/app/controllers/store_controller.rb
+++ project02/app/controllers/store_controller.rb
@@ -8,6 +8,7 @@ class StoreController < ApplicationController
   	product = Product.find(params[:id])
   	@cart = find_cart
   	@cart.add_product(product)
+  	redirect_to_index
   rescue
   	logger.error("Attempt to access invalid product #{params[:id]}")
   	redirect_to_index("Invalid product")
@@ -23,8 +24,8 @@ private
 		session[:cart] ||= Cart.new
 	end
 	
-	def redirect_to_index(msg)
-		flash[:notice] = msg
+	def redirect_to_index(msg = nil)
+		flash[:notice] = msg if msg
 		redirect_to :action => 'index'
 	end
 

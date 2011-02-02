2297edb24a4c9c3dba6659073f2acb26b49508a2
Rory Olsen <rolsen@mines.edu>
2011-02-02 05:32:15 -0700

    Fixed code for user.rb

--- project02/app/models/user.rb
+++ project02/app/models/user.rb
@@ -9,19 +9,15 @@ class User < ActiveRecord::Base
 	
 	validate :password_non_blank
 	
-private
-	
-	def password_non_blank
-		errors.add(:password, "Missing password") if hashed_password.blank?
-	end
-	
-	def self.encrypted_password(password, salt)
-		string_to_hash = password + "wibble" + salt
-		Digest::SHA1.hexdigest(string_to_hash)
-	end
-	
-	def create_new_salt
-		self.salt = self.object_id.to_s + rand.to_s
+	def self.authenticate(name, password)
+		user = self.find_by_name(name)
+		if user
+			expected_password = encrypted_password(password, user.salt)
+			if user.hashed_password != expected_password
+				user = nil
+			end
+		end
+		user
 	end
 	
 	def password
@@ -35,14 +31,18 @@ private
 		self.hashed_password = User.encrypted_password(self.password, self.salt)
 	end
 	
-	def self.authenticate(name, password)
-		user = self.find_by_name(name)
-		if user
-			expected_password = encrypted_password(password, user.salt)
-			if user.hashed_password != expected_password
-				user = nil
-			end
-		end
-		user
+private
+	
+	def password_non_blank
+		errors.add(:password, "Missing password") if hashed_password.blank?
+	end
+	
+	def self.encrypted_password(password, salt)
+		string_to_hash = password + "wibble" + salt
+		Digest::SHA1.hexdigest(string_to_hash)
+	end
+	
+	def create_new_salt
+		self.salt = self.object_id.to_s + rand.to_s
 	end
 end

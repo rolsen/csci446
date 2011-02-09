1344bc341691f4fd533138d0e53bd7d6be96987d
Rory Olsen <rolsen@mines.edu>
2011-02-08 20:00:14 -0700

    Implemented no-sally validation

--- project04/app/controllers/article_controller.rb
+++ project04/app/controllers/article_controller.rb
@@ -7,9 +7,18 @@ class ArticleController < ApplicationController
 	end
 	
 	def new
+	  @article = Article.new
 	end
 	
 	def create
+		@article = Article.new(params[:article])
+	
+	    if @article.save
+	      redirect_to(@article, :flash => { :success => 'Article was successfully created.' })
+	    else
+	      flash[:error] = 'There was a problem creating the article.'
+	      render :action => "new"
+	    end
 	end
 	
 	def edit
--- project04/app/models/article.rb
+++ project04/app/models/article.rb
@@ -1,6 +1,11 @@
 class Article < ActiveRecord::Base
   validates_presence_of :title
-  validates_uniqueness_of :title
   validates_presence_of :author
   validates_presence_of :fulltext
+  validates :no_sallys
+  
+protected
+  def no_sallys
+    errors.add_to_base("No authors named Sally are allowed.") if author.include? /sally/i
+  end  
 end

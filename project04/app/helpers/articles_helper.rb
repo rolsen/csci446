745bc655518183c594a9f6b6cbd6cc1b226a8e02
Rory Olsen <rolsen@mines.edu>
2011-02-21 01:42:25 -0700

    Fixed links in index

--- project04/app/helpers/articles_helper.rb
+++ project04/app/helpers/articles_helper.rb
@@ -1,25 +1,25 @@
 module ArticlesHelper
-	def back_link_image
-		link_to image_tag('arrow_left.png'), articles_path 
+	def all_articles_link_image
+		link_to image_tag('arrow_left.png'), articles_path, :title => 'all articles'
 	end
 
-	def back_link
-		link_to 'list all articles', articles_path 
+	def all_articles_link
+		link_to 'list all articles', articles_path, :title => 'all articles'
 	end
 	
 	def edit_link_image
-		link_to image_tag('application_edit.png'), edit_article_path 
+		link_to image_tag('application_edit.png'), edit_article_path, :title => 'edit article'
 	end
 	
 	def edit_link
-		link_to 'edit', edit_article_path 
+		link_to 'edit', edit_article_path, :title => 'edit article'
 	end
 	
 	def delete_link_image
-		link_to image_tag('delete.png'), @article, :confirm => 'Are you sure?', :method => 'delete' 
+		link_to image_tag('delete.png'), @article, :confirm => 'Are you sure?', :method => 'delete', :title => 'delete article'
 	end
 	
 	def delete_link
-		link_to 'delete', @article, :confirm => 'Are you sure?', :method => 'delete' 
+		link_to 'delete', @article, :confirm => 'Are you sure?', :method => 'delete', :title => 'delete article'
 	end
 end

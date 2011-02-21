ad3bbbffaef529a5c24f13315e5f2779de41e18b
Rory Olsen <rolsen@mines.edu>
2011-02-20 17:26:06 -0700

    Implemented validation for article creation

--- project04/app/models/article.rb
+++ project04/app/models/article.rb
@@ -1,2 +1,13 @@
 class Article < ActiveRecord::Base
+	validates_presence_of :title
+	validates_presence_of :author
+	validates_presence_of :fulltext
+	validate :not_sally
+	
+private
+	def not_sally
+		if author.downcase.include? 'sally'
+			errors.add_to_base "Author names may not contain sally. You can thank a certain someone for this..."
+		end
+	end
 end
